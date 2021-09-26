253ef1e3541f0201dc5f7b4cd894cbe061ef3e78
bannadas <bannadaso39@gmail.com>
2021-09-26 20:02:17 +0600

    add people

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,37 +1,37 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faShoppingCart} from '@fortawesome/free-solid-svg-icons'
-import {FaFacebookSquare,FaInstagram} from "react-icons/fa"
+import { faShoppingCart } from '@fortawesome/free-solid-svg-icons'
+import { FaFacebookSquare, FaInstagram } from "react-icons/fa"
 import './Users.css'
 
 const Users = (props) => {
     const cartIcon = <FontAwesomeIcon icon={faShoppingCart} />
 
-    const {image,name,post,age,hometown,salary} = props.user;
+    const { image, name, post, age, hometown, salary } = props.user;
     return (
         // card section
         <div className="users">
             <div className="user">
-            <img src={image} alt="" /> 
-           <h2>Name: {name}</h2> 
-           <h3>Post: {post}</h3> 
-           <h3>Age: {age}</h3> 
-           <h3>Hometown: {hometown}</h3> 
-           <h3>Salary:${salary}</h3> 
+                <img src={image} alt="" />
+                <h2>Name: {name}</h2>
+                <h3>Post: {post}</h3>
+                <h3>Age: {age}</h3>
+                <h3>Hometown: {hometown}</h3>
+                <h3>Salary:${salary}</h3>
             </div>
-            <button onClick={()=>props.handleAddToCartBtn(props.user)} className="add-to-cart-btn">{cartIcon} Add To Cart</button>
+            <button onClick={() => props.handleAddToCartBtn(props.user)} className="add-to-cart-btn">{cartIcon} Add People</button>
 
             {/* icons */}
             <div className="social-icons">
-            <div className="facebook-icon">
-            <FaFacebookSquare/>
+                <div className="facebook-icon">
+                    <FaFacebookSquare />
+                </div>
+                <div className="insta-icon">
+                    <FaInstagram />
+                </div>
             </div>
-            <div className="insta-icon">
-            <FaInstagram/>
-            </div>
-            </div>
-            
-           
+
+
         </div>
     );
 };
